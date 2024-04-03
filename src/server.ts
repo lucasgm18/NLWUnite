2294af106de0e21ffa680cfbf62afc747f486cce
Lucas <lucasgomesm1808@gmail.com>
2024-04-03 10:21:38 -0300

    chore: criando rotas get de teste

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import fastify from "fastify";
 
 const app = fastify()
 
+app.get('/', () => {
+    return 'Olá NLW UNITE'
+})
+
+app.get('/teste', () => {
+    return 'Hello teste'
+})
+
 app.listen({ port: 3333}).then(() => {
     console.log('Servidor HTTP ligado!')
 
