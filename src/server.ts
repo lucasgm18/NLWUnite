13bb98f1eaec14f1f9995da9048112fbada3ccf9
Lucas <lucasgomesm1808@gmail.com>
2024-04-05 13:01:46 -0300

    deletando rotas teste

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,7 @@ import fastify from "fastify";
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Olá NLW UNITE'
-})
 
-app.get('/teste', () => {
-    return 'Hello teste'
-})
 
 app.post('/users', () => {
     return 'Hello teste'
